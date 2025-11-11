cc42cb848e96c3779b32b2bd298517cab8eb3788
DiskKun <luca5h1905@gmail.com>
2025-11-10 21:17:24 -0500

    cinemachine and basic testing platforms set up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
-
+using Unity.Cinemachine;
 public class PlayerController : MonoBehaviour
 {
 
-    Vector2 direction;
-    Rigidbody2D rb;
+
     public float speed;
     public float jumpHeight;
+
+    Vector2 direction;
+    Rigidbody2D rb;
+    CinemachineCamera cam;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
         rb = GetComponent<Rigidbody2D>();
+        cam.Target.TrackingTarget = transform;
     }
 
     // Update is called once per frame
