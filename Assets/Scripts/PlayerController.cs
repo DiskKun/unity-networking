160cf3c2613aaf8d9cd5d39e1d0a850b208a1901
DiskKun <luca5h1905@gmail.com>
2025-11-10 22:16:52 -0500

    added comments to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,9 +21,11 @@ public class PlayerController : NetworkBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
         rb = GetComponent<Rigidbody2D>();
+
+        // Since start is called whenever a new player joins, ensure that this object is the owner before setting the camera target
+        // else the camera will be set to this player for all connected players
+        cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
         if (IsOwner)
         {
             cam.Target.TrackingTarget = transform;
@@ -37,32 +39,39 @@ public class PlayerController : NetworkBehaviour
         // walking
         direction = new Vector2(Input.GetAxis("Horizontal"), 0);
 
+        // jumping
         RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 0.51f, Vector2.down, 0.1f);
         bool onGround = hit;
-        // jumping
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
+            // inital jump key event
             jumping = true;
         }
         if (Input.GetKey(KeyCode.Space) && jumpTimer < maxJumpTime && jumping)
         {
+            // continued presses ticks up the jumptimer
             jumpTimer += Time.deltaTime;
             
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            // drop the jumptimer when the key is lifted
             jumpTimer = 0;
         }
         if (jumpTimer >= maxJumpTime || jumpTimer == 0)
         {
+            // quit jumping if the jumptimer is dropped or if it has exceeded the max jump time
             jumping = false;
         }
     }
 
     private void FixedUpdate()
     {
+        // horizontal movement
         rb.AddForce(direction * Time.deltaTime * acceleration);
         rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxHorizontalSpeed, maxHorizontalSpeed);
+
+        // jumping
         if (jumping)
         {
             rb.linearVelocityY = minJumpHeight;
