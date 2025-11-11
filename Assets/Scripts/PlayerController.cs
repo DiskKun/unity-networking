a234925222f8e3284ec60915c11a4e32753aa0b1
DiskKun <luca5h1905@gmail.com>
2025-11-10 22:11:19 -0500

    movement with variable jump height added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using Unity.Cinemachine;
-public class PlayerController : MonoBehaviour
+using Unity.Netcode;
+using Unity.Networking;
+using Unity.Services.Multiplayer;
+public class PlayerController : NetworkBehaviour
 {
 
 
-    public float speed;
-    public float jumpHeight;
+    public float acceleration;
+    public float maxHorizontalSpeed;
+    public float minJumpHeight;
+    public float maxJumpTime;
 
+    float jumpTimer = 0;
+    bool jumping = false;
     Vector2 direction;
     Rigidbody2D rb;
     CinemachineCamera cam;
@@ -14,23 +21,52 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
         rb = GetComponent<Rigidbody2D>();
-        cam.Target.TrackingTarget = transform;
+        if (IsOwner)
+        {
+            cam.Target.TrackingTarget = transform;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // walking
         direction = new Vector2(Input.GetAxis("Horizontal"), 0);
-        if (Input.GetAxisRaw("Vertical") == 1)
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 0.51f, Vector2.down, 0.1f);
+        bool onGround = hit;
+        // jumping
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            rb.linearVelocityY = jumpHeight;
+            jumping = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && jumpTimer < maxJumpTime && jumping)
+        {
+            jumpTimer += Time.deltaTime;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpTimer = 0;
+        }
+        if (jumpTimer >= maxJumpTime || jumpTimer == 0)
+        {
+            jumping = false;
         }
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(direction * Time.deltaTime * speed);
+        rb.AddForce(direction * Time.deltaTime * acceleration);
+        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxHorizontalSpeed, maxHorizontalSpeed);
+        if (jumping)
+        {
+            rb.linearVelocityY = minJumpHeight;
+
+        }
     }
 }
