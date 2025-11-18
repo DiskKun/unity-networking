1c93f52efa5df0d27f3256ba7b90c2ec5b285485
DiskKun <luca5h1905@gmail.com>
2025-11-17 19:57:20 -0500

    player names display

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using Unity.Cinemachine;
 using Unity.Netcode;
-using Unity.Networking;
-using Unity.Services.Multiplayer;
+using TMPro;
+
 public class PlayerController : NetworkBehaviour
 {
 
@@ -12,88 +12,128 @@ public class PlayerController : NetworkBehaviour
     public float minJumpHeight;
     public float maxJumpTime;
 
+    public TextMeshPro playerNameTextMesh;
+
+
     float jumpTimer = 0;
     bool jumping = false;
     Vector2 direction;
     Rigidbody2D rb;
     CinemachineCamera cam;
     GameManager gm;
+    ConnectionManager cm;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
-        // Since start is called whenever a new player joins, ensure that this object is the owner before setting the camera target
-        // else the camera will be set to this player for all connected players
-        cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
-        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
         if (IsOwner)
         {
+            rb = GetComponent<Rigidbody2D>();
+
+            // Since start is called whenever a new player joins, ensure that this object is the owner before setting the camera target
+            // else the camera will be set to this player for all connected players
+            cam = GameObject.Find("CinemachineCamera").GetComponent<CinemachineCamera>();
+            gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+            cm = GameObject.Find("NetworkManager").GetComponent<ConnectionManager>();
+            SetNameTextRpc(cm._profileName);
+
+            //playerNameTextMesh.text = cm._profileName;
+
             cam.Target.TrackingTarget = transform;
+
             if (IsSessionOwner)
             {
                 gm.StartRound();
             }
         }
+        
+
+
     }
+
+
+
+    [Rpc(SendTo.Everyone)]
+    void SetNameTextRpc(string name)
+    {
+        playerNameTextMesh.text = name;
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        if (IsOwner)
+        {
+            SetNameTextRpc(cm._profileName);
+        }
     }
 
     void Movement()
     {
-        // walking
-        direction = new Vector2(Input.GetAxis("Horizontal"), 0);
+        if (IsOwner)
+        {
+            // walking
+            direction = new Vector2(Input.GetAxis("Horizontal"), 0);
 
-        // jumping
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 0.51f, Vector2.down, 0.1f);
-        bool onGround = hit;
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
-        {
-            // inital jump key event
-            jumping = true;
-        }
-        if (Input.GetKey(KeyCode.Space) && jumpTimer < maxJumpTime && jumping)
-        {
-            // continued presses ticks up the jumptimer
-            jumpTimer += Time.deltaTime;
+            // jumping
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 0.51f, Vector2.down, 0.1f);
+            bool onGround = hit;
+            if (Input.GetKeyDown(KeyCode.Space) && onGround)
+            {
+                // inital jump key event
+                jumping = true;
+            }
+            if (Input.GetKey(KeyCode.Space) && jumpTimer < maxJumpTime && jumping)
+            {
+                // continued presses ticks up the jumptimer
+                jumpTimer += Time.deltaTime;
 
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                // drop the jumptimer when the key is lifted
+                jumpTimer = 0;
+            }
+            if (jumpTimer >= maxJumpTime || jumpTimer == 0)
+            {
+                // quit jumping if the jumptimer is dropped or if it has exceeded the max jump time
+                jumping = false;
+            }
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            // drop the jumptimer when the key is lifted
-            jumpTimer = 0;
-        }
-        if (jumpTimer >= maxJumpTime || jumpTimer == 0)
-        {
-            // quit jumping if the jumptimer is dropped or if it has exceeded the max jump time
-            jumping = false;
-        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Coin")
+        if (IsOwner)
         {
-            Destroy(collision.gameObject);
+            if (collision.tag == "Coin")
+            {
+                Destroy(collision.gameObject);
+            }
         }
+
     }
 
     private void FixedUpdate()
     {
-        // horizontal movement
-        rb.AddForce(direction * Time.deltaTime * acceleration);
-        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxHorizontalSpeed, maxHorizontalSpeed);
-
-        // jumping
-        if (jumping)
+        if (IsOwner)
         {
-            rb.linearVelocityY = minJumpHeight;
+            // horizontal movement
+            rb.AddForce(direction * Time.deltaTime * acceleration);
+            rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxHorizontalSpeed, maxHorizontalSpeed);
 
+            // jumping
+            if (jumping)
+            {
+                rb.linearVelocityY = minJumpHeight;
+
+            }
         }
+
     }
 }
